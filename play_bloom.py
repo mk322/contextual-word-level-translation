b4f251eff6ff39c4eb3bfb64d340a7cd863e93d4
Murray Kang <77368272+mk322@users.noreply.github.com>
2022-10-16 00:40:16 -0400

    Add files via upload

---
+++ play_bloom.py
@@ -0,0 +1,25 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
+
+tokenizer = AutoTokenizer.from_pretrained("bigscience/bloom-1b7")
+model = AutoModelForCausalLM.from_pretrained("bigscience/bloom-1b7", return_dict_in_generate=True)
+
+target_lang = "French"
+result_txt = "result_bloom.txt"
+
+source_word_list = []
+result_list = []
+with open("test.txt", encoding="utf-8") as t:
+    for word in t.read().splitlines():
+        source_word_list.append(word)
+
+with open(result_txt, "w", encoding='cp850') as r:
+    for source_word in source_word_list:
+        input_ids = tokenizer(f"In {target_lang}, the word '{source_word}' means ", return_tensors="pt").input_ids
+        result = model.generate(input_ids, do_sample=False, max_length=1, output_scores=True)
+        gen_sequences = result.sequences[:, input_ids.shape[-1]:]
+        probs = torch.stack(result.scores, dim=1).softmax(-1)
+        gen_probs = torch.gather(probs, 2, gen_sequences[:, :, None]).squeeze(-1)
+        decoded_text = tokenizer.decode(result.sequences[0])
+        print(decoded_text, file=r)
+        print(f"Score: {gen_probs[0][0]}", file=r)
