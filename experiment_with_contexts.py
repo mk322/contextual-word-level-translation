3c792dc6521dd86542579f951991bf02d1a4a7d3
Murray Kang <haoqik@cs.washington.edu>
2022-11-20 20:23:23 -0800

    Latest Update

--- experiment_with_contexts.py
+++ experiment_with_contexts.py
@@ -98,17 +98,28 @@ for source_word in words_dict.keys():
 
 # Find 3 metrics
 
-# Metric 1: calculated percentage of examples that correct translations are on the top k.
-num_correct_examples = len(words_dict) * 2
+# Metric 1a: calculated percentage of examples that the top 1 translation is correct.
+num_correct_top1 = len(words_dict) * 2
+for source_word in words_dict.keys():
+    for j in range(0, len(words_dict[source_word]), 2):
+        topk_translations = sorted(result_dict[(source_word, j)], key=lambda key: key[1], reverse=True)[0]
+        if topk_translations[0] not in words_dict[source_word][j]:
+            num_correct_top1 -= 1
+            break
+percent_correct_top1 = num_correct_top1 / (len(words_dict) * 2)
+
+
+# Metric 1b: calculated percentage of examples that correct translations are on the top k.
+num_correct_topk = len(words_dict) * 2
 for source_word in words_dict.keys():
     for j in range(0, len(words_dict[source_word]), 2):
         topk_translations = sorted(result_dict[(source_word, j)], key=lambda key: key[1], reverse=True)[:len(words_dict[source_word][j])]
         for top_pair in topk_translations:
             if top_pair[0] not in words_dict[source_word][j]:
-                num_correct_examples -= 1
+                num_correct_topk -= 1
                 break
 
-percent_correct_examples = num_correct_examples / (len(words_dict) * 2)
+percent_correct_topk = num_correct_topk / (len(words_dict) * 2)
 
 # Metric 2: What is the average log likelihood score of the correct translations (across pairs) vs. 
 # average across all other word (non correct) pairs
@@ -142,7 +153,8 @@ for source_word in words_dict.keys():
 
 # print out the results
 with open(result_txt, "w", encoding='utf-8') as r:
-    print(f"Metric 1: {round(percent_correct_examples, 6)}", file=r)
+    print("Metric 1:", file=r)
+    print(f"top1: {round(percent_correct_top1, 6)}; topk: {round(percent_correct_topk, 6)}", file=r)
     print("Metric 2", file=r) 
     print(f"average correct log-likelihood: {avg_log_correct}; average wrong log-likelihood: {avg_log_wrong}", file=r)
     print("Metric 3", file=r)
