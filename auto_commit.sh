f2f65ad13511c7905655659c66a047fd7b0237e2
Murray Kang <haoqik@uw.edu>
2023-01-19 14:54:55 -0800

    1.19

---
+++ auto_commit.sh
@@ -0,0 +1,3 @@
+C:/Users/10494/anaconda3/envs/cse446/python.exe c:/Users/10494/OneDrive/Desktop/Airdrop/uw/Research/NLP/contextual-word-level-translation-1/print_bn.py
+git commit -m "push all_senses file" all_sense_label.txt
+git push
