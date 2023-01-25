9a667f551038a4a34d22e6d8d640be3c1ebebbc7
Murray Kang <haoqik@uw.edu>
2023-01-25 14:57:29 -0800

    push all rest languages

--- auto_commit.sh
+++ auto_commit.sh
@@ -1,3 +1,4 @@
 C:/Users/10494/anaconda3/envs/cse446/python.exe c:/Users/10494/OneDrive/Desktop/Airdrop/uw/Research/NLP/contextual-word-level-translation-1/print_bn.py
-git commit -m "push all_senses file" all_sense_label.txt
+git add -A
+git commit -am "push all rest languages"
 git push
