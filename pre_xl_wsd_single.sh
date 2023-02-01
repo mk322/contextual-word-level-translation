bfb0327a97b4e9b1e813b01f4e86081982ff69eb
Murray Kang <haoqik@uw.edu>
2023-02-01 15:08:55 -0800

    Chinese target language

---
+++ pre_xl_wsd_single.sh
@@ -0,0 +1,12 @@
+tlang="en"
+full_lang="Italian"
+key="it"
+#["de"]=German ["et"]=Estonian 
+
+C:/Users/10494/anaconda3/envs/cse446/python.exe pre_xl_wsd.py --lang=$key --full_lang=$full_lang --tlang=$tlang
+C:/Users/10494/anaconda3/envs/cse446/python.exe print_bn.py --lang=$key --full_lang=$full_lang --tlang=$tlang
+#git add -A
+#git commit -am "push all rest languages"
+#git push
+#C:/Users/10494/anaconda3/envs/cse446/python.exe pre_xl_wsd.py --lang=$lang --full_lang=$full_lang --tlang=$tlang
+#C:/Users/10494/anaconda3/envs/cse446/python.exe print_bn.py --lang=$lang --full_lang=$full_lang --tlang=$tlang
