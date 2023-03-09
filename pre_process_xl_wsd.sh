b0e44455d943fc5098f551f5fe5a84beaf31ee33
Murray Kang <haoqik@uw.edu>
2023-03-09 13:14:16 -0800

    English Preprocess Done

--- pre_process_xl_wsd.sh
+++ pre_process_xl_wsd.sh
@@ -1,14 +1,16 @@
 declare -A arr
-arr["bg"]=Bulgarian
-arr+=( ["hr"]=Croatian ["da"]=Danish ["ja"]=Japanese ["nl"]=Dutch ["fr"]=French ["gl"]=Galician ["hu"]=Hungarian ["it"]=Italian)
-arr+=( ["ko"]=Korean ["sl"]=Slovenian ["de"]=German ["et"]=Estonian ["zh"]=Chinese ["eu"]=Basque ["ca"]=Catalan)
+arr["en"]=English
+#arr+=( ["hr"]=Croatian ["da"]=Danish ["ja"]=Japanese ["nl"]=Dutch ["fr"]=French ["gl"]=Galician ["hu"]=Hungarian ["it"]=Italian)
+#arr+=( ["ko"]=Korean ["sl"]=Slovenian ["de"]=German ["et"]=Estonian ["zh"]=Chinese ["eu"]=Basque ["ca"]=Catalan)
 
-tlang=es
 for key in ${!arr[@]}; do
     echo ${key} ${arr[${key}]}
+    for tlang in es en
+    do
     #C:/Users/10494/anaconda3/envs/cse446/python.exe pre_xl_wsd.py --lang=${key} --full_lang=${arr[${key}]} --tlang=$tlang
     C:/Users/10494/anaconda3/envs/cse446/python.exe print_bn.py --lang=${key} --full_lang=${arr[${key}]} --tlang=$tlang
+    done
 done
 git add -A
-git commit -am "Chinese target language"
+git commit -am "English Preprocess Done"
 git push
